cc72f052091b532d29871db26baebb8279beed33
augustine <augustino0890@gmail.com>
2023-06-08 19:00:55 +0900

    basic: String and char

---
+++ basic/Biography.java
@@ -0,0 +1,16 @@
+public class Biography {
+    public static void main(String[] args) {
+        String name = "Augustine";
+        int age = 34;
+        String country = "Vietnam";
+        String sport = "Football";
+        int hours = 2;
+        String game = "Chess";
+        String subject = "Computer Science";
+        char grade = 'A';
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from "+country+". ");
+        System.out.println("My favourite sport is "+sport+". I play for "+hours+" hours a day");
+        System.out.println("When I'm tired, I like to play "+ game+".");
+        System.out.println("In school, my favourite subject was "+subject+", I scored a "+grade+".");
+    }
+}
